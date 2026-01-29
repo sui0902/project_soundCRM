12708ddf35c125f191e3542622be2f7f9e0b9d07
sui0902 <miisaatoo892@gmail.com>
2026-01-29 10:54:54 +0900

    add:Itemのダミーデータを追加

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +15,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+                'name' => 'アコギ',
+                'memo' => '初心者から中級者まで幅広く愛用されているモデル。温かみのある音色が特徴。',
+                'price' => 45000,
+            ],
+            [
+                'name' => '電子ピアノ',
+                'memo' => '88鍵盤、3本ペダル付き。リアルなピアノタッチを再現した人気モデル。',
+                'price' => 89800,
+            ],
+            [
+                'name' => 'ドラムセット',
+                'memo' => '5ピース構成のエントリーモデル。シンバル・ハードウェア一式付属。',
+                'price' => 78000,
+            ]
+        ]);
     }
 }
