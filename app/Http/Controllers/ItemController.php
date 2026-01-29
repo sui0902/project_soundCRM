287f93ac033bb4c442fb67ea21a461eaf211fa3a
sui0902 <miisaatoo892@gmail.com>
2026-01-29 15:51:41 +0900

    feat:商品削除処理を追加

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -73,14 +73,14 @@ class ItemController extends Controller
         ]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Item  $item
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Item $item)
     {
-        //
+        $item->delete();
+
+        return to_route('items.index')
+        ->with([
+            'message' => '削除しました。',
+            'status' => 'danger'
+        ]);
     }
 }
--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -5,4 +5,7 @@
   <div v-if="$page.props.flash.status === 'success'" class="bg-amber-400 text-white p-4 rounded-lg shadow-md mb-4 w-full">
     {{ $page.props.flash.message }}
   </div>
+  <div v-if="$page.props.flash.status === 'danger'" class="bg-gray-400 text-white p-4 rounded-lg shadow-md mb-4 w-full">
+    {{ $page.props.flash.message }}
+  </div>
 </template>
