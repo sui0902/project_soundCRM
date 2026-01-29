06ebcd42dd028357833368bc5a9a34c6200a49f6
sui0902 <miisaatoo892@gmail.com>
2026-01-29 10:38:32 +0900

    add:リソースコントローラのルート定義を追加

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('items', ItemController::class)
+->middleware(['auth', 'verified']);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
