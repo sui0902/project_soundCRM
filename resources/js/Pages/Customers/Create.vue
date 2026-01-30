7ba2f87646d968cb2a47fa9bc0b780d65b5e5e2a
sui0902 <miisaatoo892@gmail.com>
2026-01-30 13:57:18 +0900

    fix:郵便番号から住所取得するように修正

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -4,6 +4,7 @@ import { Head } from '@inertiajs/vue3';
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 import InputError from '@/Components/InputError.vue';
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 defineProps({
   errors: Object
@@ -20,6 +21,12 @@ const form = reactive({
   gender: null,
   memo: null
 })
+
+const fetchAddress = () => {
+  new YubinBangoCore(String(form.postcode), (value) => {
+    form.address = value.region + value.locality + value.street
+  })
+}
 
 const storeCustomer = () => {
   Inertia.post('/customers', form)
@@ -78,7 +85,7 @@ const storeCustomer = () => {
                                 <div class="p-2 w-full">
                                   <div class="relative">
                                     <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                    <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-white focus:ring-2 focus:ring-cyan-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <input type="number" id="postcode" name="postcode" @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-cyan-500 focus:bg-white focus:ring-2 focus:ring-cyan-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     <InputError :message="errors.postcode"/>
                                   </div>
                                 </div>
