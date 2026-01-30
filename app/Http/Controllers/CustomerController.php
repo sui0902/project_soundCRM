d6b3c0176525a90989862e836bdf73197b67e885
sui0902 <miisaatoo892@gmail.com>
2026-01-30 11:27:00 +0900

    add:paginationを追加

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,7 +12,7 @@ class CustomerController extends Controller
     public function index()
     {
         return Inertia::render('Customers/Index', [
-            'customers' => Customer::select('id', 'name', 'kana', 'tel')->get()
+            'customers' => Customer::select('id', 'name', 'kana', 'tel')->paginate(50)
         ]);
     }
 
--- resources/js/Pages/Customers/Index.vue
+++ resources/js/Pages/Customers/Index.vue
@@ -2,9 +2,10 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head , Link } from '@inertiajs/vue3';
 import FlashMessage from '@/Components/FlashMessage.vue';
+import Pagination from '@/Components/Pagination.vue'
 
 defineProps({
-    customers: Array
+    customers: Object
 })
 
 </script>
@@ -40,7 +41,7 @@ defineProps({
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr v-for="customer in customers" :key="customer.id">
+                                            <tr v-for="customer in customers.data" :key="customer.id">
                                                 <td class="border-t-2 border-gray-200 px-4 py-3">{{ customer.id }}</td>
                                                 <td class="border-t-2 border-gray-200 px-4 py-3">{{ customer.name }}</td>
                                                 <td class="border-t-2 border-gray-200 px-4 py-3">{{ customer.kana }}</td>
@@ -49,7 +50,9 @@ defineProps({
                                         </tbody>
                                     </table>
                                 </div>
-                                
+                                <div class="lg:w-2/3 w-full mx-auto mt-8">
+                                    <Pagination class="mt-6" :links="customers.links"></Pagination>
+                                </div>
                             </div>
                             </section>
                     </div>
