72c0d7e7ee1878f8772bbe84e341d36d55a0a0a3
sui0902 <miisaatoo892@gmail.com>
2026-01-30 13:46:51 +0900

    fix:customerテーブル修正

--- database/migrations/2026_01_30_102019_create_customers_table.php
+++ database/migrations/2026_01_30_102019_create_customers_table.php
@@ -21,9 +21,9 @@ return new class extends Migration
             $table->string('email');
             $table->string('postcode');
             $table->string('address');
-            $table->date('birthday')->nulable();
+            $table->date('birthday')->nullable();
             $table->tinyInteger('gender'); // 0男性, 1女性、2その他
-            $table->text('memo')->nulable();
+            $table->text('memo')->nullable();
             $table->timestamps();
         });
     }
